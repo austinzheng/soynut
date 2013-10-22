0d6c88999f185d2c85697d96aa4b6211df57ab0d
conwarez <theconmethod@gmail.com>
2013-10-21 23:43:12 -0700

    Initialize clock to external oscillator, unmodified. More closely (not by much!) generate the A4 pitch using a more accurate calculation.

--- main.c
+++ main.c
@@ -4,16 +4,24 @@
  * main.c
  */
 
-#include "inc/lm4f120h5qr.h"
+#include "inc/lm4f120h5qr.h"	// LM4F120H5QR: our board
+#include "inc/hw_types.h"		// Register datatypes
+#include "driverlib/sysctl.h"	// Clock setup
+
 
 int main(void) {
-	
+    // Use the external ("main") oscillator
+    // Don't divide that oscillation.
+    // Don't multiply that oscillation.
+	// Oscillates at 16MHz.
+    SysCtlClockSet(SYSCTL_OSC_MAIN | SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_XTAL_16MHZ);
+
 	// Enable port F
 	SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
 
 	long int i;
 	for (i=0; i<10000; i++) {
-		// Let the peripheral initialize
+		// Let the peripherals initialize
 		;
 	}
 
@@ -26,13 +34,15 @@ int main(void) {
 		// Don't do anything.
 		GPIO_PORTF_DATA_R |= 0x10;
 
-		for (i=0; i<1650; i++) {
+		// 440Hz (A4): (16*10^6 cycle / 1 sec) * (1 loop / 11 cycle) * (1 sec / 880 transition)
+		// -> 1652 loop / transition
+		for (i=0; i<1652; i++) {
 			;
 		}
 
 		GPIO_PORTF_DATA_R &= ~(0x10);
 
-		for (i=0; i<1650; i++) {
+		for (i=0; i<1652; i++) {
 			;
 		}
 	}
